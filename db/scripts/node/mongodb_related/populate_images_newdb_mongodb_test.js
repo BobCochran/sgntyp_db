33204eda66b3a4462e1ae057e4b9eb4dd5f8d415
Bob Cochran <r2cochran2@gmail.com>
2014-04-10 19:09:46 -0400

    Attempt to create images collection stimages

---
+++ db/scripts/node/mongodb_related/populate_images_newdb_mongodb_test.js
@@ -0,0 +1,347 @@
+/*************************************************************************
+ * NODE.JS SCRIPT TO POPULATE A MONGODB 'stimages' collection of images  *
+ * 2014-04-10                                                            *
+ *                                                                       *
+ * To start the script, use this command line in a shell window:         *
+ * node populate_image_credits_newdb_mongodb_pho_url.js startline endline*
+ *                                                                       *
+ * for example                                                           *
+ *                                                                       *
+ * node populate_image_credits_newdb_mongodb_pho_url.js 1 240            *
+ *                                                                       *
+ * Tells the script to start processing at line 1 and terminate          *
+ * processing at line 240. Depending on your system and available memory,*
+ * 240 open file descriptors at one time may be too many to have, so     *
+ * process fewer lines per pass.                                         *
+ *                                                                       *
+ * The script processes in small passes to account for system overhead in*
+ * using up too many file descriptors.                                   *
+ * ----------------------------------------------------------------------*
+ * 2014-03-30                                                            *
+ * CUSTOMER-SUPPLIED FILE MAY NEED REFORMATTING PRIOR TO USE             *
+ * ----------------------------------------------------------------------*
+ * Read the converted_New_Names_2014-03-08_FileNamesandCaptionKeys.txt   *
+ * file as a readable stream.                                            *
+ * The input file has been reformatted from a Microsoft Windows formatted*
+ * file containing carriage returns and line feed characters '\r\n' to a *
+ * Unix style file with only '\n' terminating each line. Also, empty tab-*
+ * separated lines in the format '\t\t\t\t\n' appended to the end of the *
+ * original source input file were removed.                              *
+ * The goal in this script is to extract the filename contained in each  *
+ * row of the source file and append the extension .txt (period,         *
+ * character t, character x, character t) to this name. Then read this   *
+ * *.txt file with fs.read. Parse this file and extract the              *
+ * "Photo URL" field within the file. Next, update the matching          *
+ * document in the 'fnck' collection of the 'sgtypdb2' database          *
+ * to add this new field to the document: 'phourl'.                      *
+ *-----------------------------------------------------------------------*
+ * FOR TESTING PURPOSES, the 'fnck' collection has been copied to the    *
+ * 'test' database using mongodump and then mongorestore.                *
+ *-----------------------------------------------------------------------*
+ * The ultimate reason for updating the fnck collection in this way is to*
+ * allow 1-4 documents to be used as the source data to populate most of *
+ * each eventual application web page. Each document queried from fnck   *
+ * matches a particular image that will appear on the web page. There    *
+ * will be 4 images on each web page. See the front-end project          *
+ * repository for details.                                               *
+ *------------------------- S T E P S -----------------------------------*
+ *                                                                       *
+ * 1. Extract filename from the current input line of the                *
+ *    'converted_New_Names_2014-03-08_FileNamesandCaptionKeys.txt' file. *
+ *    Example: _8758450914_o                                             *
+ * 2. Save this filename prefix to a multidimensional array.             *
+ * 3. Append a '.txt' extension to the extracted filename.               *
+ *    _8758450914_o.txt                                                  *
+ * 4. Save the [filename].txt element to the same multimensional array as*
+ *    in step 2. The array element now looks like                        *
+ *    [["_8758450914_o", "_8758450914_o.txt",...]]                       *
+ * 5. Iterate to the next line of the FilenamesandCaptionKeys input file *
+ *    listed in Step 1. Repeat steps 1-4, building up the multi-         *
+ *    dimensional array with the elements for the next line of the input *
+ *    file. So element #1 will look like this:                           *
+ *    [["_8758450914_o", "_8758450914_o.txt",...],                       *
+ *     ["_9758450914_o", "_9758450914_o.txt",...]...]                    *
+ *    Do this in passes of 25 to 240 files.                              *
+ *                                                                       *
+ * 6. Now, working from the very beginning of the multidimensional array *
+ *    that we have built, attempt to open a file matching the            *
+ *    Filename.txt string that is at that index in our array. Example:   *
+ *    open '_8758450914_o.txt'                                           *
+ * 7. Attempt to read the file if the open succeeds.                     *
+ *    read '_8758450914_o.txt'                                           *
+ * 8. Find the line that begins with the words "Photo URL : "            *
+ * 9. Extract the photo URL.                                             *
+ *    http://www.flickr.com/myphoto                                      *
+ *10. Add the URL to the multidimensional array as in step 4. The array  *
+ *    now looks like:                                                    *
+ *[["_8758450914_o", "_8758450914_o.txt", "http://www.flickr.com/myphoto"],
+ *    ["_9758450914_o", "_9758450914_o.txt",...]...]                     *
+ *11. Iterate through all the array elements that need the photo URL     *
+ *    added to the associated filename.txt string.                       *
+ *12. With 25 to 240 filenames ready in the array, open a MongoDB        *
+ *    connection to the 'fnck' collection of either the 'test' or        *
+ *    the 'sgtypdb2' databases. At first use the 'test' database when    *
+ *    testing application logic.                                         *
+ *13. This script will attempt to update each matching document within   *
+ *    the 'fnck' collection of the 'sgtypdb2' database with the          *
+ *    URL information just extracted from the text files.                *
+ *    If the URL field is new, it will be added to the matching          *
+ *    document. The script will otherwise update the URL if the field is *
+ *    already present on the matched document.                           *
+ *-----------------------------------------------------------------------*
+ * Target database engine: MONGODB 2.4.9                                 *
+ * Required node.js module: mongodb                                      *
+ * It can take time for this script to complete all the inserts for the  *
+ * fnck collection. If you abort the script by pressing CTRL-D or        *
+ * CTRL-C the database inserts will most likely not complete.            *
+ ************************************************************************/
+var fs = require('fs');
+var totalLines = 0
+var j1 = 0              // first dimenion of im_array
+var j2 = 0              // second dimension of im_array
+var stream              // for use in parsing for photographer name
+var startLn = 0         // the starting line for this processing "pass"
+var endLn = 0           // the ending line for this processing "pass"
+var actLines = 0        // the actual number of text lines that readLines has found
+var webRows = 0         // count of web page row numbers, starting from 1
+var array_lines = 0     // the number of array iterations we need
+
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+
+var host = process.env['MONGO_NODE_DRIVER_HOST'] != null ? process.env['MONGO_NODE_DRIVER_HOST'] : 'localhost';
+var port = process.env['MONGO_NODE_DRIVER_PORT'] != null ? process.env['MONGO_NODE_DRIVER_PORT'] : 27017;
+
+/* Were any arguments passed in? Exit if none found */
+
+if (process.argv.length < 3) {
+    console.log('You must pass in the starting and ending line numbers as arguments to this script')
+    console.log('The number of arguments found were ' + process.argv.length)
+    console.log('Terminating processing')
+    process.exit(1)
+}
+
+/* Get the command line arguments passed to this script */
+
+process.argv.forEach(function(val, index, array) {
+    console.log(index + ': ' + val);
+    if (index === 2) {
+        startLn = val
+        webRows = val
+    }
+    if (index == 3) {
+        endLn = val
+    }
+});
+
+array_lines = ((endLn - startLn) + 1)      //compute number of iterations
+
+console.log('startLn is ' + startLn)
+console.log('endLn is ' + endLn)
+console.log('Number of iterations in the array will be ' + array_lines + '\n')
+
+/* Set up an [lines being processed] x 3 array */
+
+var im_array = []             // multidimensional array of image names, file names, photographer names
+for(i = 0; i < array_lines; i++) {
+    im_array[i] = [];
+    for (var y = 0; y < 3; y++) {
+        im_array[i][y] = ""
+    }
+}
+
+var input = fs.createReadStream('/Users/bobc/Documents/sgntyp_renamed_files_2014-03-08/converted_New_Names_2014-03-08_FileNamesandCaptionKeys.txt');
+
+readLines(input, func);
+get_credits()
+/*
+ * Wait 46 seconds to let other processes complete, then build
+ * the summary web page. We want to pass it the starting line number value.
+ */
+setTimeout(function () {
+    do_array_print()
+    do_db_updates()
+},46000)
+
+
+function readLines(input, func) {
+      var remaining = '';
+
+      input.on('data', function(data) {
+      remaining += data;
+      var index1 = remaining.indexOf('\n');
+      while (index1 > -1) {
+          var line = remaining.slice(0, index1);
+          remaining = remaining.slice(index1 + 1);
+          totalLines += 1
+          // Skip the header line.
+          if (totalLines === 1) {
+             index1 = remaining.indexOf('\n')
+             continue
+          }
+          actLines += 1                           // actual number of content lines minus header line
+
+          //discover if the line we are on is the line to start processing
+          if (actLines < startLn) {
+              index1 = remaining.indexOf('\n')
+              continue
+          }
+
+          //discover if the line we are on is past the ending line number
+          //that we want to process for this pass
+          if (actLines > endLn) {
+              index1 = remaining.indexOf('\n')
+              continue
+          }
+
+          index1 = remaining.indexOf('\n')        // recompute value of index1
+          var tbbl = line.indexOf('\t')           // find the index of the tab character
+          if (line[0] === '\"') {                 // check to see if the filename string is quoted
+              var fn1 = line.slice(1,(tbbl-1))    //skip the quotes
+          } else {
+              var fn1 = line.slice(0, tbbl)        // save the file name string up to the tab character
+          }
+          debugger
+          im_array[j1][j2] = fn1              // save the filename string without an extension
+
+          fn1 = fn1 + '.txt'                      // append the .txt extension
+          j2++                                    // bump to next element in j1
+          im_array[j1][j2] = fn1                  // push this onto the array
+          j2 = 0                                  // reset j2
+          j1++                                    // set up the next array
+          debugger
+
+  }
+})
+
+  input.on('end', function() {
+    if (remaining.length > 0) {
+      func(remaining);
+    }
+    console.log('\nTotal number of lines processed including input header row: ' + totalLines + '\n')
+
+  })
+    }
+/*
+ * At this point, we have extracted all the filenames from the source TSV document and these
+ * are waiting for us in the array im_array. The array now looks like this:
+ *
+ * [["_8758450914_o", "_8758450914_o.txt", ...],["_9758450914_o", "_9758450914_o.txt",...]...]
+ *
+ * For each file name in the array[j1][j2], we want to call a function that will
+ * create a readstream of that file's contents, and parse out the
+ * photo URL. The photo URL would be put into im_array[j1][2].
+ *
+ * After all the file names are processed, we can update the 'fnck' collection documents with
+ * corresponding photographer information.
+ *
+ */
+function get_credits() {
+
+    setTimeout(function () {
+        process.stdout.write('\nProcessing files...\n')
+        for(var pj = 0; pj < im_array.length; pj++) {
+            process.stdout.write(im_array[pj][0] + '... ')
+            get_photo_info(im_array[pj][1],pj)
+            process.stdout.write('done!\n')
+        }
+    }, 35000)
+
+
+}
+/*
+ This function accepts an input file name in the format filename.txt from function
+ get_credits() and attempts to create a readstream of the file. Then it attempts to
+ parse out the photographer's name and save that to im_array[pj][2].
+ */
+function get_photo_info(fname,idx) {
+
+    stream = fs.createReadStream('/Volumes/pictures/Signtyp/promptsnormalizedOnefolder/' + fname)
+    stream.on("error", function(err) {
+
+        return console.error("open file error " + err.message)
+
+    })
+    stream.on("open", function(fd) {
+        fs.fstat(fd, function(err, stats) {
+            var size
+            if (err) {
+                return console.error("fstat error: " + err.message)
+
+            } else {
+                size = stats.size
+
+
+            }
+        })
+    })
+    stream.on("data", function(data1) {
+        var chunk
+        var phline
+        var the_url
+        var url_end
+
+        chunk += data1
+        phline = chunk.indexOf('Photo URL    : ')
+        if (phline > -1) {
+            url_end = chunk.indexOf('\r',phline)
+            the_url = chunk.slice(phline+15,url_end)
+            im_array[idx][2] = the_url
+        } else {
+            console.log('Photo URL not found')
+        }
+
+
+    })
+}
+
+function func(data) {
+  console.log('Line: ' + totalLines + ' ' + data);
+ 
+}
+function do_array_print() {
+    console.log('Image name\tFilename\t\tPhoto URL\n')
+    for (var i = 0; i < im_array.length; i++) {
+        console.log(im_array[i][0] + '\t' + im_array[i][1] + '\t\t' + im_array[i][2])
+    }
+}
+function do_db_updates() {
+    /* At this point, we now have an array all ready to update the collection 'fnck' in the
+     * test or production database. We will use db.fnck.update() to add a new key to the fnck
+     * collection named 'pho'. For each document, this key will have the value of the individual
+     * photographer's name.
+     *
+     */
+
+    console.log("\nUpdating database collection.");
+    console.log("\nConnecting to database server on " + host + ":" + port +"\n");
+
+    /* Connect to the 'test' database to test out the logic. Turn on journaling. */
+
+    MongoClient.connect(format("mongodb://%s:%s/test?journal=true", host, port), function(err, db) {
+        if (err) {
+            throw err
+            return
+        }
+
+        var collection = db.collection('fnck')
+
+        for (var i = 0; i < im_array.length; i++) {
+            /* collection.update essentially adds a new key called 'phourl' */
+            collection.update(
+                { fn : im_array[i][0] },
+                { $set: {
+                    phourl : im_array[i][2]}
+                }, function (err, result) {
+                    if (err) return err
+                })
+
+            /* wait 20 seconds and hope write operations are done */
+            setTimeout(function () {
+                db.close()
+            },20000)
+        }
+
+    })
+
+}
